afc807aba1e498614dec489c9d69b3d553bc9e5b
Soleneperchet <solene.perchet@gmail.com>
2020-04-13 17:22:55 +0200

    création carousel

--- header.php
+++ header.php
@@ -1,81 +1,165 @@
 <!DOCTYPE html>
- <html lang="en">
- <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,900;1,400&display=swap"
-        rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet">
-     <link rel="stylesheet" href="wp-content/themes/sculpture/style.css">
- </head>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,900;1,400&display=swap"
+    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="wp-content/themes/sculpture/style.css">
+</head>
+
 <body>
+
+
+<!-- <div class="container-fluid container-header p-0">
+
+
+
+
+<div class=" col-11 img-header-accueil vh-100 d-flex align-items-end justify-content-center p-0">
+  
+  <?php 
+       $image = get_field('image_header_accueil');
+       $size = 'full'; // (thumbnail, medium, large, full or custom size)
+       if( $image ) {
+           echo wp_get_attachment_image( $image, $size );
+       }
+
+       ?>
+       <div class="text-header-absolute">
+       <h1><?php the_field('titre_header'); ?></h1>
+       </div>
+      </div> 
+
+
+
+
+<div class="w-100 d-flex justify-content-end p-0">
+
+<div class="logo-helmet bg-dark-logo text-center">
+<?php 
+       $image = get_field('logo_helmet');
+       $size = 'full'; // (thumbnail, medium, large, full or custom size)
+       if( $image ) {
+           echo wp_get_attachment_image( $image, $size );
+       }
+
+       ?>
+
+</div>
+
+<div class="menu vh-100 d-flex align-items-start p-0">
+   <?php wp_nav_menu( array( 'theme_location' => 'menu-burger' ) ); ?>
+</div>
+</div>
+
+
+
+</div> -->
 
 
 
 <div class="container-fluid container-header p-0">
 
-                 <div class="menu-header">
-                    <?php wp_nav_menu( array( 'theme_location' => 'menu-burger' ) ); ?>
-                </div>
 
 
-                <div class=" col-11 img-header-accueil vh-100 d-flex align-items-end justify-content-center p-0">
-                   
-                   <?php 
-                        $image = get_field('image_header_accueil');
-                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
-                        if( $image ) {
-                            echo wp_get_attachment_image( $image, $size );
-                        }
 
-                        ?>
-                        <div class="text-header-absolute">
-                        <h1><?php the_field('titre_header'); ?></h1>
-                        </div>
-                       </div> 
-              
+<div class="w-25 col-11 img-header-accueil vh-100 d-flex align-items-end  p-0">
+  
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">01</li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1">02</li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2">03</li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3">04</li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <?php 
+       $image = get_field('image_header_accueil');
+       $size = 'full'; // (thumbnail, medium, large, full or custom size)
+       if( $image ) {
+           echo wp_get_attachment_image( $image, $size );
+       }
 
-       
-
-            <div class="container-fluid d-flex justify-content-end p-0">
-            
-                <div class="logo-helmet bg-dark-logo text-center">
-                <?php 
-                        $image = get_field('logo_helmet');
-                        $size = 'full'; // (thumbnail, medium, large, full or custom size)
-                        if( $image ) {
-                            echo wp_get_attachment_image( $image, $size );
-                        }
-
-                        ?>
-   
+       ?>
+       <div class="text-header-absolute">
+       <h1><?php the_field('titre_header'); ?></h1>
+       </div>
     </div>
+    <div class="carousel-item">
+    <?php 
+       $image = get_field('image_header_accueil');
+       $size = 'full'; // (thumbnail, medium, large, full or custom size)
+       if( $image ) {
+           echo wp_get_attachment_image( $image, $size );
+       }
+
+       ?>
+       <div class="text-header-absolute">
+       <h1><?php the_field('titre_header'); ?></h1>
+       </div>
+    </div>
+    <div class="carousel-item">
+    <?php 
+       $image = get_field('image_header_accueil');
+       $size = 'full'; // (thumbnail, medium, large, full or custom size)
+       if( $image ) {
+           echo wp_get_attachment_image( $image, $size );
+       }
+
+       ?>
+       <div class="text-header-absolute">
+       <h1><?php the_field('titre_header'); ?></h1>
+       </div>
+    </div>
+  </div>
+ 
+</div>
+      </div>
+
+
+
+
+<div class="w-100 d-flex justify-content-end p-0">
+
+<div class="logo-helmet bg-dark-logo text-center">
+<?php 
+       $image = get_field('logo_helmet');
+       $size = 'full'; // (thumbnail, medium, large, full or custom size)
+       if( $image ) {
+           echo wp_get_attachment_image( $image, $size );
+       }
+
+       ?>
 
 </div>
 
-               
-            
-        </div>
+<div class="menu vh-100 d-flex align-items-start p-0">
+   <?php wp_nav_menu( array( 'theme_location' => 'menu-burger' ) ); ?>
+</div>
+</div>
+
+
+
+</div>
 
 
 
 
-
-
-
-
-
-
-
-                <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
